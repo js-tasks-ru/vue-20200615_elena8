1624017de0781f4d2de3f8acc40f12e4d77e5528
Berdnik Elena <e.berdnik@amio.ru>
2020-06-25 18:59:14 +0300

    fix 2-2

--- 2-module/2-task/MeetupAgendaItem.js
+++ 2-module/2-task/MeetupAgendaItem.js
@@ -9,7 +9,7 @@ export const MeetupAgendaItem = {
               </div>
               <div class="meetup-agenda__item-col">{{agendaItem.startsAt}} - {{agendaItem.endsAt}}</div>
               <div class="meetup-agenda__item-col">
-                <h5 class="meetup-agenda__title" v-html="title"></h5>
+                <h5 class="meetup-agenda__title">{{title}}</h5>
                 <p v-if="agendaItem.type === 'talk'">
                   <span>{{agendaItem.speaker}}</span>
                   <span class="meetup-agenda__dot" />

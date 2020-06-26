68565164bea6089501257d31e6e9d4c8490ec15a
Berdnik Elena <e.berdnik@amio.ru>
2020-06-27 02:57:14 +0300

    solution for 3-1

---
+++ 3-module/1-task/components/CounterButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <button type="button" @click="handleCount">{{count}}</button>
+</template>
+
+<script>
+  export default {
+    name: 'counter-button',
+    props: {
+      count: {
+        type: Number,
+        default: 0
+      }
+    },
+    data: function () {
+      return {
+        counter: this.count
+      }
+    },
+    methods: {
+      handleCount: function() {
+        this.counter++
+        this.$emit('increment', this.counter)
+      }
+    },
+    model: {
+      prop: 'count',
+      event: 'increment'
+    },
+
+  }
+</script>

f668547f6b728f52ff39dd2e18e0136238e5c6ce
Berdnik Elena <e.berdnik@amio.ru>
2020-06-27 18:50:05 +0300

    solution 3-2

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
-const vueTasks = [[3, 1]];
+const vueTasks = [[3, 2]];
 
 const pages = Object.fromEntries(
   vueTasks.map(([module, task]) => [
     `${module}-module-${task}-task`,
     {
-      entry: `./${module}-module/${task}-task/src/main.js`,
-      template: `./${module}-module/${task}-task/src/index.html`,
+      entry: `./${module}-module/${task}-task/main.js`,
+      template: `./${module}-module/${task}-task/index.html`,
     },
   ]),
 );
